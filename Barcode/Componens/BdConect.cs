0d0da72329abb7746610e817b170685573dc25de
Арсений Макаров <makki09092004@hotmail.com>
2023-02-03 10:01:21 +0300

    Добавьте файлы проекта.

---
+++ Barcode/Componens/BdConect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barcode.Componens
+{
+    class BdConect
+    {
+        public static CodeEntities bd = new CodeEntities();
+    }
+}
